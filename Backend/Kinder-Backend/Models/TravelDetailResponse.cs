6e6063eaa8c06f56daea4f3d82f4fc470e484be6
Michelle Hung <chialinghung74@gmail.com>
2022-12-24 17:17:18 +0800

    refactor: remove unused code

--- Backend/Kinder-Backend/Models/TravelDetailResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Kinder_Backend.Models
-{
-    public class TravelDetailResponse
-    {
-        public List<TravelDetail> TravelDetail { get; set; }
-    }
-}
--- Backend/Kinder-Backend/Models/TravelResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Kinder_Backend.Models
-{
-    public class TravelResponse
-    {
-        public List<TravelList> TravelList { get; set; }
-    }
-}
